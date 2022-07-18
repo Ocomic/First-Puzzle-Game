be6594630cf07c89dac1c06f47eb6626e2cdc1fb
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-07-18 16:52:40 +0200

    Door movement
    Added Version 1 of moving doors. Doors can now be dragged on 2 axis.
    Added World Builder Feature to Unity.

--- My Puzzle Game/Assets/Scenes/Scripts/DoorController.cs
+++ My Puzzle Game/Assets/Scenes/Scripts/DoorController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera myCam;
+
+    private float startXPos;
+    private float startYPos;
+
+    private bool isDragging = false;
+
+    private void Update()
     {
-        
+        if (isDragging)
+        {
+            DragObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+
+        if (!myCam.orthographic)
+        {
+            mousePos.z = 10;
+        }
+
+        mousePos = myCam.ScreenToWorldPoint(mousePos);
+
+        startXPos = mousePos.x - transform.localPosition.x;
+        startYPos = mousePos.y - transform.localPosition.y;
+
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    public void DragObject()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (!myCam.orthographic)
+        {
+            mousePos.z = 10;
+        }
+
+        mousePos = myCam.ScreenToWorldPoint(mousePos);
+        transform.localPosition = new Vector3(mousePos.x - startXPos, mousePos.y - startYPos, transform.localPosition.z);
     }
 }
